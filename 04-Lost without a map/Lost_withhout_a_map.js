719738a4f2896a538daf6a856193c229d1a32c91
Oerd Bej <oerdbej@gmail.com>
2024-01-28 23:27:54 +0100

    challenge 4 - Lost without a map - refactoring

--- 04-Lost without a map/Lost_withhout_a_map.js
+++ 04-Lost without a map/Lost_withhout_a_map.js
@@ -9,3 +9,10 @@ function maps(x) {
 const inputArray = [1, 2, 3, 4, 5, 6];
 const result = maps(inputArray);
 console.log(result);
+
+//2️⃣ solution using a map function 
+
+const maps = (x) => x.map((item) => item * 2);
+const inputArray2 = [1, 2, 3, 4, 5, 6];
+const result2 = maps(inputArray2);
+console.log(result2);
