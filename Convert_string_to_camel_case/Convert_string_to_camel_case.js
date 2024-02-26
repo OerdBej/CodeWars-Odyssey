f87e4e2a2fc5e706e0894193a3fb9ab44c653b07
Oerd Bej <oerdbej@gmail.com>
2024-02-26 20:07:17 +0100

    Convert Strings to Camel Case - refactor using loops

--- Convert_string_to_camel_case/Convert_string_to_camel_case.js
+++ Convert_string_to_camel_case/Convert_string_to_camel_case.js
@@ -13,3 +13,18 @@ function toCamelCase(str) {
 }
 
 console.log(toCamelCase("the-stealth-warrior"));
+
+
+// 2️⃣ using loop
+
+function toCamelCase(str) {
+  let arr = str.split("");
+  for (i = 0; i < arr.length; i++) {
+    let letter = arr[i];
+    if (letter == "_" || letter == "-") {
+      arr[i + 1] = arr[i + 1].toUpperCase();
+      arr[i] = "";
+    }
+  }
+  return arr.join("");
+}
